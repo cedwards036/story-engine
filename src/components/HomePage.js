c4eac7a8af8d9a1105ddc32a0ec877bf05f6ddcb
Christopher Edwards <cedwards036@gmail.com>
2021-08-03 20:44:20 -0400

    get list of decks from api in homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../App.css';
 
 export default function HomePage() {
-    return (<div>Home Page</div>)
+    const [decks, setDecks] = useState([])
+    useEffect(() => {
+        fetch(process.env.REACT_APP_API_URL + 'decks', { method: 'GET', mode: 'cors' })
+            .then(res => res.json())
+            .then(data => setDecks(data))
+    }, []);
+    return (<div>Decks: {decks.map(d => <div key={d.id}>{d.name}</div>)}</div>)
 }
